1c010873466cc0a3912cd97cb468de1b13a1c265
Kauan <kauanhindlmayer07@gmail.com>
2022-11-18 13:36:23 -0300

    feat: add Product

---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+namespace Store.Domain.Entities
+{
+  public class Product : Entity
+  {
+    public Product(string title, decimal price, bool active)
+    { 
+      Title = title;
+      Price = price;
+      Active = active;
+    }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public bool Active { get; set; }
+
+    public void ChangeInfo(string title, decimal price, bool active)
+    { 
+      Title = title;
+      Price = price;
+      Active = active;
+    }
+  }
+}
